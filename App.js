168db633919ee6329e373a989af22f8ed1c1350f
Akrep-Clean <muhammeturkoglu0@gmail.com>
2025-05-27 17:05:11 +0300

    homescreen eklendi!

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './homescreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hadi bakalimm</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <HomeScreen />
 }
 
 const styles = StyleSheet.create({
